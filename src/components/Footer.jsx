9d20e8a72c3300559a7bd80e17c8be04783e7a7a
LouisAugustine <nzlouis.com@gmail.com>
2022-05-07 19:45:10 +1200

    Add moment to get the current year.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import moment from 'moment'
 
 function Footer(props) {
 
     return (
         <div className="container">
             <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-                <p className="col-md-4 mb-0 text-muted">&copy; 2022 NZ Louis</p>
+                <p className="col-md-4 mb-0 text-muted">&copy; {moment().year()} NZ Louis</p>
                 <ul className="nav col-md-4 justify-content-end">
                 <li className="nav-item"><a href="https://nzlouis.com" target="_blank" rel="noreferrer" className="nav-link px-2 text-muted">About Me</a></li>
                 </ul>
