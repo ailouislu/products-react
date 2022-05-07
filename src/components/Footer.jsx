abccba4eb7d6908c000fc0f5a6ea599094f78069
LouisAugustine <nzlouis.com@gmail.com>
2022-05-07 19:39:38 +1200

    Delete the space

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,8 +7,6 @@ function Footer(props) {
         <div className="container">
             <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
                 <p className="col-md-4 mb-0 text-muted">&copy; 2022 NZ Louis</p>
-
-
                 <ul className="nav col-md-4 justify-content-end">
                 <li className="nav-item"><a href="https://nzlouis.com" target="_blank" rel="noreferrer" className="nav-link px-2 text-muted">About Me</a></li>
                 </ul>
